7b864d8604ec27bd2c201a70d51f68d91ac9ad92
renshimosawa <polaris2760@gmail.com>
2021-11-17 20:53:29 +0900

    fix

---
+++ Notify.py
@@ -0,0 +1,80 @@
+import schedule
+import setting
+import scraping
+import requests
+
+message=scraping.message
+
+TOKEN_F = setting.AP_F
+TOKEN_GM = setting.AP_GM
+TOKEN_N = setting.AP_N
+TOKEN_Y = setting.AP_Y
+
+def mainF():
+    send_line_notify_F(
+      message
+    )
+
+def mainGM():
+    send_line_notify_GM(
+      message
+    )
+
+def mainN():
+    send_line_notify_N(
+      message
+    )
+
+def mainY():
+    send_line_notify_Y(
+      message
+    )
+
+def send_line_notify_F(notification_message):
+    """
+    LINEに通知する
+    """
+    line_notify_token_F = TOKEN_F
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token_F}'}
+    data = {'message': f'\n{notification_message}'}
+    requests.post(line_notify_api, headers = headers, data = data)
+
+def send_line_notify_GM(notification_message):
+    """
+    LINEに通知する
+    """
+    line_notify_token_GM = TOKEN_GM
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token_GM}'}
+    data = {'message': f'\n{notification_message}'}
+    requests.post(line_notify_api, headers = headers, data = data)
+
+def send_line_notify_N(notification_message):
+    """
+    LINEに通知する
+    """
+    line_notify_token_N = TOKEN_N
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token_N}'}
+    data = {'message': f'\n{notification_message}'}
+    requests.post(line_notify_api, headers = headers, data = data)
+
+def send_line_notify_Y(notification_message):
+    """
+    LINEに通知する
+    """
+    line_notify_token_Y = TOKEN_Y
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {line_notify_token_Y}'}
+    data = {'message': f'\n{notification_message}'}
+    requests.post(line_notify_api, headers = headers, data = data)
+
+if __name__ == "__main__":
+    mainF()
+    mainGM()
+    mainN()
+    mainY()
+
+while True:
+    schedule.run_pending(5)
--- scraping.py
+++ scraping.py
@@ -1,10 +1,6 @@
 import bs4
-import time
 import requests
 import datetime
-import setting
-
-
 
 Target_URL = 'https://covid19.codeforaomori.org/'
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
@@ -15,26 +11,5 @@ soup = bs4.BeautifulSoup(resp.text, "html.parser")
 entries = soup.find_all(class_="DataView-Header")
 number = entries[0].find(class_="DataView-DataInfo-summary").text.replace('人','').replace(' ','').replace('\n', '')
 date = entries[0].find(class_="DataView-DataInfo-date").text
+
 message = f'{now}の青森県の新規感染者数は\n{number}人\n({date})'
-
-TOKEN_F = setting.AP_F
-
-def mainF():
-    send_line_notify_F(
-      message
-    )
-
-def send_line_notify_F(notification_message):
-    """
-    LINEに通知する
-    """
-    line_notify_token = TOKEN_F
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'\n{notification_message}'}
-    requests.post(line_notify_api, headers = headers, data = data)
-
-
-if __name__ == "__main__":
-    mainF()
-    time.sleep(3)
