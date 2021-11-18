2877799625f18cd3993fc7d8a4119194ac86ed0f
renshimosawa <polaris2760@gmail.com>
2021-11-18 22:23:38 +0900

    change env yml

--- setting.py
+++ setting.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-AP_F = os.environ.get("LINE_TOKEN_F")
-AP_Y = os.environ.get("LINE_TOKEN_Y")
-AP_N = os.environ.get("LINE_TOKEN_N")
-AP_GM = os.environ.get("LINE_TOKEN_GM")
-AP_ME = os.environ.get("LINE_TOKEN_MINE")
+AP_F = os.environ.get("LINE_TOKEN_F ")
+AP_Y = os.environ.get("LINE_TOKEN_Y ")
+AP_N = os.environ.get("LINE_TOKEN_N ")
+AP_GM = os.environ.get("LINE_TOKEN_GM ")
+AP_ME = os.environ.get("LINE_TOKEN_MINE ")
