05a6f1fd2fc03f67cf543ef171b5ac8e9b05aedc
renshimosawa <polaris2760@gmail.com>
2021-11-18 21:06:51 +0900

    change env yml

--- setting.py
+++ setting.py
@@ -1,9 +1,9 @@
 import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+# from os.path import join, dirname
+# from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
 AP_F = os.environ.get("LINE_TOKEN_F")
 AP_Y = os.environ.get("LINE_TOKEN_Y")
