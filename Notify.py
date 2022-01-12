3f13f7fecae1e46ef7d6f427fe0e7e4dffc2a15e
renshimosawa <polaris2760@gmail.com>
2022-01-12 19:59:12 +0900

    changed target site

--- Notify.py
+++ Notify.py
@@ -8,6 +8,7 @@ TOKEN_F = os.environ.get("LINE_TOKEN_F")
 TOKEN_Y = os.environ.get("LINE_TOKEN_Y")
 TOKEN_N = os.environ.get("LINE_TOKEN_N")
 TOKEN_GM = os.environ.get("LINE_TOKEN_GM")
+# TOKEN_MINE = os.environ.get("LINE_TOKEN_MINE")
 
 def main():
     send_line_notify_F(
@@ -22,6 +23,10 @@ def main():
     send_line_notify_Y(
       message
     )
+    # send_line_notify_ME(
+    #   message
+    # )
+
 
 
 def send_line_notify_F(notification_message):
@@ -63,6 +68,16 @@ def send_line_notify_Y(notification_message):
     headers = {'Authorization': f'Bearer {line_notify_token_Y}'}
     data = {'message': f'\n{notification_message}'}
     requests.post(line_notify_api, headers = headers, data = data)
+    
+# def send_line_notify_ME(notification_message):
+#     """
+#     LINEに通知する
+#     """
+#     line_notify_token_MINE = TOKEN_MINE
+#     line_notify_api = 'https://notify-api.line.me/api/notify'
+#     headers = {'Authorization': f'Bearer {line_notify_token_MINE}'}
+#     data = {'message': f'\n{notification_message}'}
+#     requests.post(line_notify_api, headers = headers, data = data)
 
 
 if __name__ == "__main__":
